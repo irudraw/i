a7a79a62e8205991f651934f0bba72a9c602d5b3
irudraw <41246008+irudraw@users.noreply.github.com>
2020-03-23 23:04:22 -0500

    Update index.php

--- fembed/index.php
+++ fembed/index.php
@@ -38,7 +38,7 @@ $titulo = $_GET["titulo"];
 <div id="myElement"></div>
 <div data-theme="b" align="center"> <h3><?php echo $titulo ?></h3> </div>
 
-    <script src="https://content.jwplatform.com/libraries/lOWCQSKB.js"></script>
+    <script src="https://cdn.jwplayer.com/libraries/OsHCZHvZ.js"></script>
 	<script>
 		var playerInstance = jwplayer("myElement");
 		playerInstance.setup({
