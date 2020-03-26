485f8edd74b3192a1a7ae2cb4ee524f3a9a81783
irudraw <41246008+irudraw@users.noreply.github.com>
2020-03-26 10:14:43 -0500

    Add files via upload

---
+++ anti/anti.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="anti.css"/>
+<link rel="stylesheet" href="animate.min.css"/>
+<div id="WgD">
+  <div class='WgD-position'>
+    <div class='WgD-container'>
+      <h3 class='WgD-title'>
+Adblock Detectado</h3>
+<p class='WgD-text'>
+Nuestro sitio se mantiene gracias a la publicidad, por favor <strong>Desactiva Adblock</strong> para seguir navegando</p>
+<a class='WgD-reload' href=".">He desactivado Adblock</a>
+    </div>
+</div>
+<div class='WgD-particles'>
+    <i class="wgd-usd"></i><i class="wgd-eur"></i>
+    <i class="wgd-usd"></i><i class="wgd-eur"></i>
+    <i class="wgd-btc"></i><i class="wgd-usd"></i>
+    <i class="wgd-usd"></i><i class="wgd-eur"></i>
+    <i class="wgd-btc"></i><i class="wgd-usd"></i>
+    <i class="wgd-eur"></i>
+  </div>
+</div>
+<script>//<![CDATA[
+function adBlockDetected(){document.getElementById("WgD").classList.add("is-detected"),document.body.classList.add("ab-is-detected")}if("undefined"!=typeof fuckAdBlock||"undefined"!=typeof FuckAdBlock)adBlockDetected();else{var importFAB=document.createElement("script");importFAB.onload=function(){fuckAdBlock.onDetected(adBlockDetected),fuckAdBlock.onNotDetected(adBlockNotDetected)},importFAB.onerror=function(){adBlockDetected()},importFAB.integrity="sha384-CXnSItT4vv0CF9Lrll7Wu5SofkJWovtQcEuqpH2REEeQURSJapSzBwvm1QTDwgBK",importFAB.crossOrigin="anonymous",importFAB.src="https://cdn.jsdelivr.net/npm/fuckadblock@3.2.1/fuckadblock.min.js",document.head.appendChild(importFAB)}
+//]]></script>
