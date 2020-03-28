b1fd688e1225e3d12386448f2639b4cd6bc1c245
irudraw <41246008+irudraw@users.noreply.github.com>
2020-03-27 23:06:41 -0500

    Update xhome.php

--- anime/xhome.php
+++ anime/xhome.php
@@ -168,6 +168,22 @@ html5Lightbox.showItem(href);
   </div>
 
 
+  <!-- Load Facebook SDK for JavaScript -->
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+  <!-- Your save button code -->
+  <div class="fb-save" 
+    data-uri="http://zanime.herokuapp.com/anime/Youjo%20Senki/">
+  </div>	    
+	    
+	    
 <!--    <a href="?a=estrenos" target="_parent"><button class="btn btn-danger navbar-btn">Animes en Estreno</button></a> -->
   </div>
 </nav>
