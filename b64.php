95a586777c795ae2a18c7f1af183d1ce2b0c906a
irudraw <41246008+irudraw@users.noreply.github.com>
2020-04-03 13:47:28 -0500

    Update b64.php

--- b64.php
+++ b64.php
@@ -1,5 +1,13 @@
-<?php
-$filename = $_GET["file"];
+<!DOCTYPE html>
+<html lang="esB">
+<head> 
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>B64</title>
+</head>
 
-<img src="data:image/jpg;base64,<?php echo base64_encode(file_get_contents("$filename")) ?>" height="19" width="78" alt="base64" />
-?>
+<body>
+  
+<img src="data:image/jpg;base64,<?php $filename = $_GET["file"]; echo base64_encode(file_get_contents("$filename")) ?>" height="19" width="78" alt="base64" />
+
+</body>
+</html>
