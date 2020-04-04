3b9d31d3145b317bdaa42762eb7a15cbcd55c863
irudraw <41246008+irudraw@users.noreply.github.com>
2020-04-03 21:33:33 -0500

    Update reload.php

--- reload.php
+++ reload.php
@@ -40,7 +40,29 @@ html5Lightbox.showItem(href);
 <!--finscrpgalleria-->
 
 
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 
+<script type="text/javascript">
+
+    function getTimeAJAX() {
+
+        //GUARDAMOS EN UNA VARIABLE EL RESULTADO DE LA CONSULTA AJAX    
+
+        var time = $.ajax({
+
+            url: 'index_listview.php', //indicamos la ruta donde se genera la hora
+                dataType: 'text',//indicamos que es de tipo texto plano
+                async: false     //ponemos el parámetro asyn a falso
+        }).responseText;
+
+        //actualizamos el div que nos mostrará la hora actual
+        document.getElementById("myWatch").innerHTML = time;
+    }
+
+    //con esta funcion llamamos a la función getTimeAJAX cada segundo para actualizar el div que mostrará la hora
+    setInterval(getTimeAJAX,3000);
+
+</script>
 
 
 <link href='img/zgtvlogo.png' rel='shortcut icon' type='image/png'/>
@@ -105,29 +127,7 @@ html5Lightbox.showItem(href);
   <ul data-role="listview" data-inset="true" data-filter="true" data-input="#myFilter">
 <div id='myWatch'></div>
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 
-<script type="text/javascript">
-
-    function getTimeAJAX() {
-
-        //GUARDAMOS EN UNA VARIABLE EL RESULTADO DE LA CONSULTA AJAX    
-
-        var time = $.ajax({
-
-            url: 'index_listview.php', //indicamos la ruta donde se genera la hora
-                dataType: 'text',//indicamos que es de tipo texto plano
-                async: false     //ponemos el parámetro asyn a falso
-        }).responseText;
-
-        //actualizamos el div que nos mostrará la hora actual
-        document.getElementById("myWatch").innerHTML = "La fecha que hemos obtenido de time.php vía AJAX es: "+time;
-    }
-
-    //con esta funcion llamamos a la función getTimeAJAX cada segundo para actualizar el div que mostrará la hora
-    setInterval(getTimeAJAX,1000);
-
-</script>
 
 
   </ul>
