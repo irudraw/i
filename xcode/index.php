e089a2dce5a03e4356acaa08e84e0f8c831503a3
irudraw <41246008+irudraw@users.noreply.github.com>
2020-04-11 23:53:37 -0500

    Update index.php

--- xcode/index.php
+++ xcode/index.php
@@ -2,13 +2,13 @@
 header('Content-Type: text/html; charset=utf-8');
 
 $link = $_SERVER[HTTP_HOST] . $_SERVER[REQUEST_URI];
-$linkx = $_SERVER[REQUEST_URI];
+$linkx = $_SERVER[PHP_SELF];
 
 
 $escaped_link = htmlspecialchars($link, ENT_QUOTES, 'UTF-8');
 
-echo '<a href="'.$escaped_link.'">'.$escaped_link.'</a>';
+echo '<a href="'.$escaped_link.'">'.$escaped_link.'</a><br>';
 
-echo '<a href="'.$links.'">'.links.'</a>';
+echo '<a href="'.$linkx.'">'.linkx.'</a>';
 
 ?>
