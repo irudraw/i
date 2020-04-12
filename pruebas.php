254e4295e641674e01e36e8e97d0c8dfee504e26
irudraw <41246008+irudraw@users.noreply.github.com>
2020-04-11 23:45:28 -0500

    Delete pruebas.php

--- pruebas.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-
-$link = $_SERVER[HTTP_HOST] . $_SERVER[REQUEST_URI];
-$escaped_link = htmlspecialchars($link, ENT_QUOTES, 'UTF-8');
-echo '<a href="'.$escaped_link.'">'.$escaped_link.'</a>';
-
-?>
