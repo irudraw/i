714287fc6448c91cb91d77ca7eb7827d829e7cc3
irudraw <41246008+irudraw@users.noreply.github.com>
2020-04-27 02:58:41 -0500

    Update 64x.php

--- 64x.php
+++ 64x.php
@@ -1,4 +1,5 @@
 <?php
-header('Content-Type: image/png');
+header('Content-Type: video/mp4');
 ?>
-<img src="data:image/png;base64,<?php $filename = $_GET["v"]; echo base64_encode(file_get_contents("$filename")) ?>" height="200" width="200" alt="base64" />
+<video src="https://nontongo.win/api-mega/get.php?id=!5mIgnKCQ!l0L0YEP0e3PViA7uVhmqefUOqREWh5FoEun0ds5MFwI" height="200" width="200" />
+<!-- <img src="data:image/png;base64,<?php $filename = $_GET["v"]; echo base64_encode(file_get_contents("$filename")) ?>" height="200" width="200" alt="base64" /> -->
