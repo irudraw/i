69ae66564c081b1fb92202a5775a41ce564ced8c
irudraw <41246008+irudraw@users.noreply.github.com>
2020-04-27 02:53:32 -0500

    Update 64x.php

--- 64x.php
+++ 64x.php
@@ -1,2 +1,4 @@
+<?php
 header('Content-Type: image/png');
+?>
 <img src="data:image/png;base64,<?php $filename = $_GET["v"]; echo base64_encode(file_get_contents("$filename")) ?>" height="200" width="200" alt="base64" />
